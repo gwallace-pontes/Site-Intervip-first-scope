1e0a6ef8d8d9328e8029054a3d6d45f947f4b19a
Guilherme Wallace <81390266+gwallace-pontes@users.noreply.github.com>
2021-07-15 12:51:47 -0300

    Index.php
    INDEX DA PÁGINA

---
+++ index.php
@@ -0,0 +1,136 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	<title>Intervip - Gerência de Redes</title>
+	
+	<!-- CSS -->
+	<link href="CSS/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="GerenciaIntervip/styles/style.css">
+	
+
+</head>
+<body class="geral">
+	
+	<top-header>
+		<header>
+			
+			<div class="container-fluid"> 
+				
+				<div class="row">
+					
+					<div class="col-12 col-sm-2 col-md-3 logo-wrapper">
+						<a href="index.php"><img class="logo" src="assets/logo.png"></a>
+					</div>
+				
+					<div class="col-12 col-sm-8 col-md-6" id="cabecalho">
+						<h2>Intervip - Gerência de Redes</h2>
+						<p>
+					</div>
+					<div class="col-12 col-sm-2 col-md-3">
+						
+					</div>
+				
+				</div>
+			</div>
+
+		</header>
+    </top-header>
+    
+    <main>
+
+		<div class="container-fluid">
+
+			<div class="container">
+
+				<div class="card border border-dark bg-light">
+					<div class="card-body" id="centralizar">
+						
+						<hr>
+						<div class="row">
+							<div class="col-md-4">
+							  <h3>Consulta de planos</h3>
+							  <p></p>
+							  <p><a class="btn btn-primary" href="ConsultaDePlanos" role="button">Acessar »</a></p>
+							</div>
+							<div class="col-md-4">
+							  <h3>Gerencia de Clientes</h3>
+							  <p></p>
+							  <p><a class="btn btn-primary" href="ivp-web" role="button">Acessar »</a></p>
+							</div>
+							<div class="col-md-4">
+							  <h3>Envio de Demandas para Logistica</h3>
+							  <p></p>
+							  <p><a class="btn btn-primary" href="enviar-email" role="button">Acessar »</a></p>
+							</div>
+						</div>
+						  <hr>
+						<div class="row">
+							<div class="col-md-4">
+							  <h3>Testes de Lentidão</h3>
+							  <p></p>
+							  <p><a class="btn btn-primary" href="teste_lentidao" role="button">Acessar »</a></p>
+							</div>
+							<div class="col-md-4">
+							  <h3>Mysql - PhpMyadmin</h3>
+							  <p></p>
+							  <p><a class="btn btn-primary" href="http://10.254.0.254/phpmyadmin" role="button">Acessar »</a></p>
+							</div>
+							<div class="col-md-4">
+							  <h3>Cadastros</h3>
+							  <p></p>
+							  <p><a class="btn btn-primary" href="loginCadastros" role="button">Acessar »</a></p>
+							</div>					
+						</div>
+						<!--
+						<hr>
+						<div class="row">
+							<div class="col-md-4">
+								
+								<a href="./" style="color: black;">
+								<div class="card">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+									</div>
+								</div>
+								</a>
+
+							</div>
+							<div class="col-md-4">
+
+							</div>
+							<div class="col-md-4">
+							
+							</div>					
+						</div>-->
+						  <hr>
+						  <br><br><br><br><br><br>
+					</div>
+				</div>
+			</div>
+		</div>       
+    </main>
+	
+		
+	<footer class="footer navbar-fixed-bottom">
+	
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-2"><a href="index.php"><img class="logo" src="assets/logo.png"></a></div>
+			</div>
+			<div class="row">
+				<div class="col-md-10 d-flex align-items-center">
+					<p>© 2021 Gerencia Intervip | Todos os direitos reservados a Intervip.</p>
+				</div>
+			</div>
+		</div>
+	
+	
+		<!-- JS -->
+		<script src="JS/jquery.js"></script>
+		<script src="JS/bootstrap.bundle.min.js"></script>		
+	</footer>
+</body>
+</html>
